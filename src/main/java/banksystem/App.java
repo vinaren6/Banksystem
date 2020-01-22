b9607044db37e41007fe82cb59e7b95a1a8247b2
vinaren6 <viktor.ruden2@gmail.com>
2020-01-22 18:59:59 +0100

    push project

---
+++ src/main/java/banksystem/App.java
@@ -0,0 +1,13 @@
+package banksystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/banksystem/AppTest.java
@@ -0,0 +1,47 @@
+package banksystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import banksystem.users.Admin;
+import banksystem.users.User;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void createAdmin()
+    {
+        Admin admin = new Admin("Admin", "admin1", "admin1234", "admin@admin.admin");
+        assertEquals("admin1", admin.userName);
+    }
+    // @Test
+    // public void createUser()
+    // {
+    //     User user = new User("Joshua", "Klein", "Worker", "12345","YEEET", "user@user.user", 200000000);
+    //     assertEquals("Klein" );
+    // }
+    @Test
+    public void checkUserSalary()
+    {
+        User user = new User("Joshua", "Klein", "Worker", "12345","YEEET", "user@user.user", 20000);
+        int salary = user.viewSalary();
+        assertEquals(20000, salary);
+    }
+
+    @Test
+    public void checkUserRole()
+    {
+        User user = new User("Joshua", "Klein", "Worker", "12345","YEEET", "user@user.user", 20000);
+        String role = user.viewRole();
+        assertEquals("Worker", role);
+    }
+}
